d086985d53e3469fab11b03204a8feee86f84ec0
shinedark <mide0303@gmail.com>
2017-01-12 10:04:30 -0600

    f

--- README.md
+++
@@ -1,3 +0,0 @@
-# portfolio
-# cp
-# camilopineda
---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('.box1').click(function(){
+      $('.ca ').show();
+
+    });
+    $('.container').click(function(){
+      $('.ca').hide();
+    });
+
+    $('.box2').click(function(){
+      $('.ido ').show();
+
+    });
+    $('.container').click(function(){
+      $('.ido').hide();
+    });
+
+    $('.box3').click(function(){
+      $('.cont ').show();
+
+    });
+    $('.container').click(function(){
+      $('.cont').hide();
+    });
+
+});
+
+
