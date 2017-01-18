e6fe267cf80be24dfbce4d212a4370e3bcb8aa33
shinedark <mide0303@gmail.com>
2017-01-17 21:02:29 -0600

    animations

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
+
+  $('.h1').addClass('animated bounce').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
+      function(){
+        $(this).removeClass('animated bounce');
+      });
+  $('.hard').addClass('animated hinge').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
+      function(){
+        $(this).removeClass('animated hinge');
+      });
+
     $('.box1').click(function(){
+      $('.ca ').addClass('animated rollIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
+      function(){
+        $(this).removeClass('animated rollIn');
+      });
       $('.ca ').show();
       $('.ido ').hide();
       $('.cont ').hide();
@@ -10,6 +24,10 @@ $(document).ready(function() {
     });
 
     $('.box2').click(function(){
+      $('.ido ').addClass('animated rollIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
+      function(){
+        $(this).removeClass('animated rollIn');
+      });
       $('.ido ').show();
       $('.ca ').hide();
       $('.cont ').hide();
@@ -20,6 +38,10 @@ $(document).ready(function() {
     });
 
     $('.box3').click(function(){
+       $('.cont ').addClass('animated rollIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
+      function(){
+        $(this).removeClass('animated rollIn');
+      });
       $('.cont ').show();
       $('.ca ').hide();
       $('.ido ').hide();
