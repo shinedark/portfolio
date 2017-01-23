f97a697ee571b82e951c63cc1ac3525f40c53b71
shinedark <Shine@camilos-MacBook-Pro.local>
2017-01-23 16:38:00 -0600

    2ndt

--- js/app.js
+++ js/app.js
@@ -13,53 +13,26 @@ $(document).ready(function() {
         $(this).removeClass('animated rotateOut');
       });
 
-    $('.box1').click(function(){
-      $('.ca ').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
-      function(){
-        $(this).removeClass('animated fadeInUp');
-      });
-      $('.ca ').show();
-      $('.ido ').hide();
-      $('.cont ').hide();
-      $('.hard ').hide();
+  $('.box1').click(function(){
+    $('html, body').animate({
+            scrollTop: $(".pro").offset().top
+        }, 2000);
+   
+  });
 
-    });
-    $('.container').click(function(){
-      $('.ca').hide();
-      $('.hard ').show();
-    });
+  $('.box2').click(function(){
+    $('html, body').animate({
+            scrollTop: $(".ido").offset().top
+        }, 2000);
+   
+  });
 
-    $('.box2').click(function(){
-      $('.ido ').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
-      function(){
-        $(this).removeClass('animated fadeInUp');
-      });
-      $('.ido ').show();
-      $('.ca ').hide();
-      $('.cont ').hide();
-      $('.hard ').hide();
-
-    });
-    $('.container').click(function(){
-      $('.ido').hide();
-      $('.hard ').show();
-    });
-
-    $('.box3').click(function(){
-       $('.cont ').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
-      function(){
-        $(this).removeClass('animated fadeInUp');
-      });
-      $('.cont ').show();
-      $('.ca ').hide();
-      $('.ido ').hide();
-      $('.hard ').hide();
-
-    });
-    $('.container').click(function(){
-      $('.cont').hide();
-      $('.hard ').show();
-    });
+  $('.box3').click(function(){
+    $('html, body').animate({
+            scrollTop: $(".cont").offset().top
+        }, 2000);
+   
+  });
 
 });
 
